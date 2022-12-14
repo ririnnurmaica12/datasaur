72443241e249d56d17ccb20b6edbe1fe35dcb2ad
Ririn Nurmaica <74649717+ririnnurmaica12@users.noreply.github.com>
2022-12-14 20:03:58 +0700

    Update remove and rename runner wikipedia

--- src/test/java/wikipedia/runnerWikipedia.java
+++ src/test/java/wikipedia/runnerWikipedia.java
@@ -8,6 +8,6 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = {"src/test/java/wikipedia/scenario/wikipedia.feature"}
 )
-public class runner {
+public class runnerWikipedia {
 
 }
